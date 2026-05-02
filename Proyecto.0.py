25ff0f3e2268437bcfe998c9e39961f42f7a30d5
SrJxrx <136137294+SrJxrx@users.noreply.github.com>
2026-05-01 20:14:00 -0600

    Add files via upload

---
+++ Proyecto.0.py
@@ -0,0 +1,28 @@
+def cesarCod(texto, desplazamiento):
+
+def cesarDec(texto, desplazamiento):
+
+def monoCod(texto,palabra):
+
+def monoDec(texto,palabra):
+
+def vigenereCod(texto,palabra):
+
+def vigenereDec(texto,palabra):
+
+def playfairCod(texto,palabra):
+
+def playfairDec(texto,palabra):
+
+def railfenceCod(texto):
+
+def railfenceDec(texto):
+
+def escitalaCod(texto, lineas):
+
+def escitalaDec(texto, lineas):
+
+def main():
+
+
+
