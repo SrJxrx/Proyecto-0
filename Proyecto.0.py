8f885cda14140c28c0e550d5bc0c94affb7f8067
Andrey Morales <andreymorj05@gmail.com>
2026-05-07 21:57:42 -0600

    Update Proyecto.0.py

--- Proyecto.0.py
+++ Proyecto.0.py
@@ -1,22 +1,34 @@
+#Cesar codificación y decodificación.
+
 def cesarCod(texto, desplazamiento):
 
 def cesarDec(texto, desplazamiento):
+
+#Monoalfabetico codificación y decodificación.
 
 def monoCod(texto,palabra):
 
 def monoDec(texto,palabra):
 
+#Vigenere codificación y decodificación.
+
 def vigenereCod(texto,palabra):
 
 def vigenereDec(texto,palabra):
+
+#Playfair codificación y decodificación.
 
 def playfairCod(texto,palabra):
 
 def playfairDec(texto,palabra):
 
+#Railfence codificación y decodificación.
+
 def railfenceCod(texto):
 
 def railfenceDec(texto):
+
+#Escitala codificación y decodificación.
 
 def escitalaCod(texto, lineas):
 
